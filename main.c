2fbe482cc13d8f430713c12cfa50898c40a3baf0
sxclij <sxclij@gmail.com>
2024-06-12 14:08:13 +0000

    add cmd_sl

--- main.c
+++ main.c
@@ -13,12 +13,12 @@ void file_read(char* dst, const char* src) {
     fclose(fp);
 }
 
-union uni64 {
+typedef union uni64 {
     uint64_t u64;
     uint32_t u32[2];
     char* s;
     void (*fn)(union uni64);
-};
+} uni64;
 struct token {
     uint32_t start;
     uint32_t size;
@@ -27,7 +27,7 @@ struct token {
 struct hashtable hashtable;
 struct hashtable_table {
     struct token key;
-    union uni64 value;
+    uni64 value;
 };
 struct hashtable {
     struct hashtable_table table[hashtable_capacity];
@@ -39,7 +39,7 @@ uint32_t hashtable_xorshift(uint32_t x) {
     x ^= x >> 17;
     x ^= x << 5;
 }
-union uni64* hashtable_provide(const char* src_data, uint32_t src_size) {
+uni64* hashtable_provide(const char* src_data, uint32_t src_size) {
     uint32_t hash = 0;
     for (uint32_t i = 0; i < src_size; i++) {
         hash += src_data[i];
@@ -62,18 +62,32 @@ union uni64* hashtable_provide(const char* src_data, uint32_t src_size) {
         hash = hashtable_xorshift(hash);
     }
 }
-union uni64* hashtable_provide_str(const char* src) {
+uni64* hashtable_provide_str(const char* src) {
     return hashtable_provide(src, strlen(src));
 }
 
-
+void cmd_sl(uni64 x) {
+    printf("shu po shu po\n");
+}
 
 int main() {
     static char text_data[file_capacity];
+    static char input_data[file_capacity];
 
     file_read(text_data, "data.txt");
 
-    
+    hashtable_provide_str("sl")->fn = cmd_sl;
+
+    while (1) {
+        printf("> ");
+        scanf("%s", input_data);
+        uni64* x = hashtable_provide(input_data, strlen(input_data));
+        if (x->u64) {
+            x->fn(*x);
+        } else {
+            printf("not found\n");
+        }
+    }
 
     return 0;
 }
