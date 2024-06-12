4771a88ef588e37f1145a21a8394cb7039e17859
sxclij <sxclij@gmail.com>
2024-06-12 12:55:59 +0000

    add file_read

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdint.h>
+
+#define file_capacity 65536
+
+void file_read(char* dst, const char* src) {
+    FILE* fp = fopen("data.txt", "r");
+    uint32_t n = fread(dst, 1, file_capacity, fp);
+    dst[n] = '\0';
+    fclose(fp);
+}
 
 int main() {
-    printf("good morning world.\n");
+    static char text_data[file_capacity];
+
+    file_read(text_data, "data.txt");
+
+    printf("%s\n", text_data);
+
     return 0;
 }
